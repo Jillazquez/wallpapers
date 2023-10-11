e264a561ac64cc177068fa857babdeb4630fa334
Jillazquez <jillazquez@gmail.com>
2023-10-11 19:36:59 +0200

    Update wallpaper.sh
    Update script
    Now supports cinnamon

--- wallpaper.sh
+++ wallpaper.sh
@@ -5,6 +5,10 @@ RANDOM_PICTURE=$(find "$WALLPAPER_DIR" | shuf -n 1 | xargs basename)
 CURRENT_DESKTOP="$(echo "$XDG_CURRENT_DESKTOP" | awk '{for (i=1;i<=NF;i++) { $i=toupper(substr($i,1,1)) tolower(substr($i,2)) }}1')"
 
 case "$CURRENT_DESKTOP" in
+  Cinnamon)
+    gsettings set org.cinnamon.desktop.background picture-uri "file://$WALLPAPER_DIR/$RANDOM_PICTURE"
+    gsettings set org.cinnamon.desktop.background picture-uri-dark "file://$WALLPAPER_DIR/$RANDOM_PICTURE"
+  ;;
   Gnome)
     gsettings set org.gnome.desktop.background picture-uri "file://$WALLPAPER_DIR/$RANDOM_PICTURE"
     gsettings set org.gnome.desktop.background picture-uri-dark "file://$WALLPAPER_DIR/$RANDOM_PICTURE"
